0b365dc91becad7fee2ae8cb713bb02094eb4c8e
jesderr <tlen4iki@mail.ru>
2023-10-06 16:07:12 +0300

    begin with other bot

---
+++ src/main/java/com/example/telegrambotjesderrr/TelegramBotJesderrrApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotjesderrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotJesderrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotJesderrrApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/telegrambotjesderrr/model/CurrencyModel.java
@@ -0,0 +1,16 @@
+package com.example.telegrambotjesderrr.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class CurrencyModel {
+    Integer cur_ID;
+    Date date;
+    String cur_Abbreviation;
+    Integer cur_Scale;
+    String cur_Name;
+    Double cur_OfficialRate;
+}
+
