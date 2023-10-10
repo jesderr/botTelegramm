a1186c32d879671ea8c5b9867af8de5e0a160570
jesderr <tlen4iki@mail.ru>
2023-10-11 01:23:38 +0300

    made the code more readable

--- src/main/java/com/example/telegrambotjesderrr/TelegramBot.java
+++ src/main/java/com/example/telegrambotjesderrr/TelegramBot.java
@@ -14,7 +14,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -39,9 +38,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
             switch (messageText) {
-                case "/start" -> {
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                }
+                case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                 case "read" -> {
                     userState.put(chatId, "read");
                     sendMessage(chatId, "You've chosen to read, now enter the text number.");
@@ -52,52 +49,94 @@ public class TelegramBot extends TelegramLongPollingBot {
                 }
                 default -> {
                     String state = userState.get(chatId);
-                    if ("see".equals(state)) {
-                        try {
-                            int photoNumber = Integer.parseInt(messageText);
-                            if (photoNumber > 0 && photoNumber < 101) {
+                    try {
+                        int number = Integer.parseInt(messageText);
+                        if (isValidNumber(number)) {
+                            if ("see".equals(state)) {
                                 ModelPhoto modelPhoto = new ModelPhoto();
                                 PhotoService photoService = new PhotoService(this);
-                                String url = photoService.getPhoto("photos", modelPhoto, photoNumber);
+                                String url = photoService.getPhoto("photos", modelPhoto, number);
                                 photoService.sendPhoto(chatId, url);
-                                userState.remove(chatId);
-                            } else {
-                                sendMessage(chatId, "Please enter correct number.(0-100)");
-                            }
-                        } catch (NumberFormatException e) {
-                            sendMessage(chatId, "Please enter a number to get a photo.");
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        } catch (ParseException e) {
-                            throw new RuntimeException("Unable to parse message");
-                        }
-                    } else if ("read".equals(state)) {
-                        try {
-                            int textNumber = Integer.parseInt(messageText);
-                            if (textNumber > 0 && textNumber < 101) {
+                            } else if ("read".equals(state)) {
                                 ModelMessage modelMessage = new ModelMessage();
                                 MessageService messageService = new MessageService();
-                                String resMessage = messageService.getMessage("posts", modelMessage, textNumber);
+                                String resMessage = messageService.getMessage("posts", modelMessage, number);
                                 sendMessage(chatId, resMessage);
-                                userState.remove(chatId);
-                            } else {
-                                sendMessage(chatId, "Please enter correct number.(0-100)");
                             }
-                        } catch (NumberFormatException e) {
-                            sendMessage(chatId, "Please enter a number to get a photo.");
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        } catch (ParseException e) {
-                            throw new RuntimeException("Unable to parse message");
+                            userState.remove(chatId);
+                        } else {
+                            sendMessage(chatId, "Please enter a valid number (1-100).");
                         }
-                    } else {
-                        sendMessage(chatId, "Please select the 'see' or 'read' option.");
+                    } catch (NumberFormatException e) {
+                        sendMessage(chatId, "Please enter a number.");
+                    } catch (IOException | ParseException e) {
+                        throw new RuntimeException(e);
                     }
                 }
             }
         }
     }
+//            switch (messageText) {
+//                case "/start" -> {
+//                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+//                }
+//                case "read" -> {
+//                    userState.put(chatId, "read");
+//                    sendMessage(chatId, "You've chosen to read, now enter the text number.");
+//                }
+//                case "see" -> {
+//                    userState.put(chatId, "see");
+//                    sendMessage(chatId, "You've chosen to see, now enter the photo number.");
+//                }
+//                default -> {
+//                    String state = userState.get(chatId);
+//                    if ("see".equals(state)) {
+//                        try {
+//                            int photoNumber = Integer.parseInt(messageText);
+//                            if (isValidNumber(photoNumber)) {
+//                                ModelPhoto modelPhoto = new ModelPhoto();
+//                                PhotoService photoService = new PhotoService(this);
+//                                String url = photoService.getPhoto("photos", modelPhoto, photoNumber);
+//                                photoService.sendPhoto(chatId, url);
+//                                userState.remove(chatId);
+//                            } else {
+//                                sendMessage(chatId, "Please enter correct number.(0-100)");
+//                            }
+//                        } catch (NumberFormatException e) {
+//                            sendMessage(chatId, "Please enter a number to get a photo.");
+//                        } catch (IOException e) {
+//                            throw new RuntimeException(e);
+//                        } catch (ParseException e) {
+//                            throw new RuntimeException("Unable to parse message");
+//                        }
+//                    } else if ("read".equals(state)) {
+//                        try {
+//                            int textNumber = Integer.parseInt(messageText);
+//                            if (isValidNumber(textNumber)) {
+//                                ModelMessage modelMessage = new ModelMessage();
+//                                MessageService messageService = new MessageService();
+//                                String resMessage = messageService.getMessage("posts", modelMessage, textNumber);
+//                                sendMessage(chatId, resMessage);
+//                                userState.remove(chatId);
+//                            } else {
+//                                sendMessage(chatId, "Please enter correct number.(0-100)");
+//                            }
+//                        } catch (NumberFormatException e) {
+//                            sendMessage(chatId, "Please enter a number to get a photo.");
+//                        } catch (IOException e) {
+//                            throw new RuntimeException(e);
+//                        } catch (ParseException e) {
+//                            throw new RuntimeException("Unable to parse message");
+//                        }
+//                    } else {
+//                        sendMessage(chatId, "Please select the 'see' or 'read' option.");
+//                    }
+//                }
+//            }
 
+    private boolean isValidNumber(int number) {
+        return number >= 1 && number <= 100;
+    }
 
     private void startCommandReceived(Long chatId, String name) {
         String answer = "Hi, " + name + ", nice to meet you!" + "\n" +
